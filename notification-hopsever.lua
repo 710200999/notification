fa1ca73c5b1c8b22f2f1278a74a7adb3e92b2cd9
710200999 <198722707+710200999@users.noreply.github.com>
2026-02-19 15:05:53 +0700

    Fix notification text in notification-hopsever.lua
    Updated notification text to remove period.

--- notification-hopsever.lua
+++ notification-hopsever.lua
@@ -2,8 +2,9 @@ local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCore("SendNotification", {
     Title = "DVHS_Team!"; -- The title of the notification
-    Text = "Hop Sever."; -- The description text
+    Text = "Hop Sever"; -- The description text
     Icon = "rbxassetid://6860051781"; -- Optional: Icon Asset ID
     Duration = 2; -- Time in seconds
     --Button1 = "OK"; -- Optional: Text for button 1
 })
+
