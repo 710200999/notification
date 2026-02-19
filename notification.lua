f4486f036101b395e23d222bfffd835640782b59
710200999 <198722707+710200999@users.noreply.github.com>
2026-02-19 14:50:06 +0700

    Update notification duration to 3 seconds
    Increased notification duration from 2 to 3 seconds.

--- notification.lua
+++ notification.lua
@@ -4,6 +4,7 @@ StarterGui:SetCore("SendNotification", {
     Title = "DVHS_Team!"; -- The title of the notification
     Text = "script has been activated."; -- The description text
     Icon = "rbxassetid://6860051781"; -- Optional: Icon Asset ID
-    Duration = 2; -- Time in seconds
+    Duration = 3; -- Time in seconds
     --Button1 = "OK"; -- Optional: Text for button 1
 })
+
