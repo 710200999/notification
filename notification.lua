b9ddccc82a788f5d3375134b6129f4f607fe7592
710200999 <198722707+710200999@users.noreply.github.com>
2026-02-19 15:06:15 +0700

    Update notification text formatting
    Removed period from notification text.

--- notification.lua
+++ notification.lua
@@ -2,9 +2,10 @@ local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCore("SendNotification", {
     Title = "DVHS_Team!"; -- The title of the notification
-    Text = "script has been activated."; -- The description text
+    Text = "script has been activated"; -- The description text
     Icon = "rbxassetid://6860051781"; -- Optional: Icon Asset ID
     Duration = 3; -- Time in seconds
     --Button1 = "OK"; -- Optional: Text for button 1
 })
+
 
